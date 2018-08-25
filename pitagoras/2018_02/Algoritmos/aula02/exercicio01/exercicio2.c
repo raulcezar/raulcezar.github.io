c6351aa02cf3138038f62a7f7ef3e533534150e8
raulcezar <raulcezar@gmail.com>
2018-08-25 17:06:24 -0300

    up

---
+++ pitagoras/2018_02/Algoritmos/aula02/exercicio01/exercicio2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int A,B,X;
+    printf("Digite 2 valores 2 \n");
+    scanf("%d", &A);
+    scanf("%d", &B);
+    X = A + B;
+    printf("Soma = %.2f \n", X);
+    return 0;
+}
